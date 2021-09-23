53cac3dc04c5f4ae2c673d06628d78d03355d159
Rafael de Andrade Sousa <rafael.andrade@tendermint.com>
2021-09-23 09:00:27 -0400

    Applying nolinting

--- x/distribution/legacy/v043/helpers.go
+++ x/distribution/legacy/v043/helpers.go
@@ -19,7 +19,7 @@ func MigratePrefixAddress(store sdk.KVStore, prefixBz []byte) {
 
 	for ; oldStoreIter.Valid(); oldStoreIter.Next() {
 		addr := oldStoreIter.Key()
-		var newStoreKey []byte = prefixBz
+		var newStoreKey [] byte = prefixBz	// nolint
 		newStoreKey = append(newStoreKey, address.MustLengthPrefix(addr)...)
 
 		// Set new key on store. Values don't change.
